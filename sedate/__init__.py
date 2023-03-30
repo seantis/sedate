8f6ddc9299a6b0dae3c02c2da26e238d915de7b0
David Salvisberg <david.salvisberg@seantis.ch>
2023-03-30 10:47:01 +0200

    Fixes up inclusion of py.typed marker

--- sedate/__init__.py
+++ sedate/__init__.py
@@ -36,7 +36,7 @@ if TYPE_CHECKING:
     from .types import TzInfo
     from .types import TzInfoOrName
 
-__version__ = '1.0.2'
+__version__ = '1.0.2.post1'
 
 mindatetime = pytz.utc.localize(datetime.min)
 maxdatetime = pytz.utc.localize(datetime.max)
